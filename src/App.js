d32dcf6cd26c456dfb626608913d5145ce897419
Daniel <dibanezalegria@gmail.com>
2021-10-07 15:20:16 +0200

    added react-bootstrap and list for docs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup";
 
 import axios from "axios";
 
@@ -12,39 +19,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
-      content: ""
+      title: "<Document Name>",
+      content: "",
+      list: []
     };
-    this.handleLoad = this.handleLoad.bind(this);
     this.handleSave = this.handleSave.bind(this);
     this.handleTitleChange = this.handleTitleChange.bind(this);
-  }
-
-  componentDidMount() {
-    axios
-      .get(apiURL)
-      .then((res) => {
-        this.setState({
-          title: res.data.docs[0].title,
-          content: res.data.docs[0].content
-        });
-      })
-      .catch((error) => console.log(error));
-  }
-
-  handleLoad(e) {
-    e.preventDefault();
-    axios
-      .get(apiURL + this.state.title)
-      .then((res) => {
-        if (res.data.doc) {
-          this.setState({
-            title: res.data.doc.title,
-            content: res.data.doc.content
-          });
-        }
-      })
-      .catch((error) => console.log(error));
+    this.handleListEvent = this.handleListEvent.bind(this);
   }
 
   handleSave(e) {
@@ -53,50 +34,91 @@ class App extends Component {
       title: this.state.title,
       content: this.state.content
     };
-
     axios
       .put(apiURL, doc)
-      .then((res) => console.log(res))
+      .then((res) => {
+        this.updateList();
+      })
       .catch((error) => console.log(error));
   }
 
-  handleTitleChange(event) {
-    this.setState({ title: event.target.value });
+  handleTitleChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  handleListEvent(e) {
+    e.preventDefault();
+    axios
+      .get(apiURL + e.target.value)
+      .then((res) => {
+        if (res.data.doc) {
+          this.setState({
+            title: res.data.doc.title,
+            content: res.data.doc.content
+          });
+          this.updateList();
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  updateList() {
+    axios
+      .get(apiURL)
+      .then((res) => {
+        const list = res.data.docs.map((doc) => (
+          <ListGroup.Item
+            key={doc._id}
+            value={doc.title}
+            action
+            onClick={this.handleListEvent}
+          >
+            {doc.title}
+          </ListGroup.Item>
+        ));
+        this.setState({
+          list: list
+        });
+      })
+      .catch((error) => console.log(error));
   }
 
   render() {
     return (
-      <div>
-        <h2>Using CKEditor 5 build in React</h2>
-        <div className="navbar">
-          <label>
-            Title:
-            <input
-              type="text"
-              value={this.state.title}
-              onChange={this.handleTitleChange}
-            />
-          </label>
-          <button type="button" onClick={this.handleLoad}>
-            Load
-          </button>
-          <button type="button" onClick={this.handleSave}>
-            Save
-          </button>
-        </div>
-
-        <CKEditor
-          editor={ClassicEditor}
-          data={this.state.content}
-          onReady={(editor) => {}}
-          onChange={(event, editor) => {
-            const newContent = editor.getData();
-            this.setState({ content: newContent });
-          }}
-          onBlur={(event, editor) => {}}
-          onFocus={(event, editor) => {}}
-        />
-      </div>
+      <Container>
+        <Row>
+          <Col lg={8}>
+            <Row>
+              <InputGroup>
+                <FormControl
+                  value={this.state.title}
+                  onChange={this.handleTitleChange}
+                />
+                <Button onClick={this.handleSave} variant="outline-secondary">
+                  Save
+                </Button>
+              </InputGroup>
+            </Row>
+            <Row>
+              <CKEditor
+                className="editor"
+                editor={ClassicEditor}
+                data={this.state.content}
+                onReady={(editor) => {this.updateList();}}
+                onChange={(event, editor) => {
+                  const newContent = editor.getData();
+                  this.setState({ content: newContent });
+                }}
+                onBlur={(event, editor) => {}}
+                onFocus={(event, editor) => {}}
+              />
+            </Row>
+          </Col>
+          <Col lg={3}>
+            <ListGroup>{this.state.list}</ListGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
